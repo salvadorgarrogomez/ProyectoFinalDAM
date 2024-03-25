1c47123a2eb74601473b5d1e101ab3a2f2d69a8d
salvadorgarrogomez <salvaxggz@gmail.com>
2024-03-25 08:40:27 +0100

    Avances APP Movil 25/03 PC

--- APP/app/src/main/java/com/example/app_movilproyecto/Mesa.java
+++ APP/app/src/main/java/com/example/app_movilproyecto/Mesa.java
@@ -1,2 +1,20 @@
-package com.example.app_movilproyecto;public class Mesa {
+package com.example.app_movilproyecto;
+
+public class Mesa {
+    private int numero;
+    private String estado;
+
+    public Mesa(int numero, String estado) {
+        this.numero = numero;
+        this.estado = estado;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
 }
+
