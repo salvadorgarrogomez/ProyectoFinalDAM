c320efd9ddf7118430c9630db779eac4d5a06b4f
salvadorgarrogomez <salvaxggz@gmail.com>
2024-05-30 21:59:29 +0200

    App-PC 30/05

--- api-comandas/src/main/java/com/example/api_comandas/repositorios/ProductosRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.example.api_comandas.repositorios;
-
-import com.example.api_comandas.entidades.Productos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductosRepositorio extends JpaRepository<Productos, Long>{
-}
